220ec295d920c52592972ca5c5d4677141f7ca46
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:38:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ကိုနဲ့ POP Server ကိုမူဝါဒများ';
+$lang['imap_plugin_app_description'] = 'IMAP ကိုနဲ့ POP Server ကိုမူဝါဒများအ IMAP ကိုနဲ့ POP Server ကို app ကိုများအတွက်ဝင်ရောက်ခွင့်ကိုထိန်းချုပ်ပေး။';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ကိုနဲ့ POP Server ကိုအသုံးပြုသူတို့၏';
