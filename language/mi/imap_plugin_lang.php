15226eec89f53a980060da740034a9f06f206f4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:43:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'Kaupapa here Tūmau IMAP me te POP';
+$lang['imap_plugin_app_description'] = 'whakarato IMAP me Tūmau POP kaupapa here mana uru mo te IMAP me POP taupānga Tūmau.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP me Tūmau POP Kaiwhakamahi';
