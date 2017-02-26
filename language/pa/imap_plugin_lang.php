167a955164d4a5e0fc6c1901f7daec39f68b9e01
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:44:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ਅਤੇ POP ਸਰਵਰ ਡਰਾਇਰ';
+$lang['imap_plugin_app_description'] = 'IMAP ਅਤੇ POP ਸਰਵਰ ਡਰਾਇਰ IMAP ਅਤੇ POP ਸਰਵਰ ਐਪ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ਅਤੇ POP ਸਰਵਰ ਲਈ ਯੂਜ਼ਰ';
