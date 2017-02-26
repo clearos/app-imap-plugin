bff969780db2d61244c8905afee6a91a0a839454
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP agus POP Polasaithe Freastalaí';
+$lang['imap_plugin_app_description'] = 'a chur ar fáil IMAP agus Freastalaí POP Polasaithe rialaithe rochtana don IMAP agus POP app Freastalaí.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP agus Freastalaí POP Úsáideora';
