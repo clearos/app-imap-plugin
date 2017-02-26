4520dd6f4679cd97dce03070188d12ec95ac483a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:47:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP at POP Patakaran Server';
+$lang['imap_plugin_app_description'] = 'IMAP at POP Server Patakaran magbigay ng access control para sa IMAP at POP Server app.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP at POP Server User';
