b842c6c699a92422688bb494e46e851b013bda05
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:47:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ва POP Сиёсати Server';
+$lang['imap_plugin_app_description'] = 'IMAP ва POP Server сиёсат таъмини идоракунии дастрасӣ, барои IMAP ва POP барнома Server.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ва POP Server User';
