c8d6ca4a95bc789fa6534537d489fb8611b25e9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:38:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ndi Pop Ndondomeko Server';
+$lang['imap_plugin_app_description'] = 'IMAP ndi Pop Server Ndondomeko imapatsa kulamulira kwa app IMAP ndi Pop Server.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ndi Pop Server Wosuta';
