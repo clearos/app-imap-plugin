dd260faf9364cc8fc486da734e14b0868811a3f2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:48:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'Chính sách IMAP và POP server';
+$lang['imap_plugin_app_description'] = 'IMAP và POP server Chính sách cung cấp kiểm soát truy cập cho các ứng dụng IMAP và POP Server.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP và POP server tài';
