3f46d2181f2fe315537ca5f7259d0f92ab82f9e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:37:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'سياسات خادم IMAP و POP';
+$lang['imap_plugin_app_description'] = 'توفر IMAP و POP خادم سياسات التحكم في الوصول للIMAP و POP التطبيق خادم.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP و POP خادم العضو';
