158bcd38f38270c5027f791822146b0d26a4d632
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:48:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'Polisïau Gweinydd IMAP a POP';
+$lang['imap_plugin_app_description'] = 'IMAP a Gweinydd POP Polisïau darparu rheolaeth mynediad ar gyfer yr IMAP a POP app Gweinydd.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP a Gweinydd POP Defnyddiwr';
