7d6b13b71a90a9b6acf6659976c376016ef6f48d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:43:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP dan POP Polisi Server';
+$lang['imap_plugin_app_description'] = 'IMAP dan POP Server Polisi menyediakan kawalan akses untuk aplikasi IMAP dan POP Server.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP dan POP Server Pengguna';
