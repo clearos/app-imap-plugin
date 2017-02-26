97a0031fd417a25b9f251d57160a1f70a6d6fd7c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:41:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'គោលនយោបាយ IMAP និងម៉ាស៊ីនបម្រើ POP';
+$lang['imap_plugin_app_description'] = 'ម៉ាស៊ីនបម្រើ POP IMAP និងគោលនយោបាយផ្តល់ការត្រួតពិនិត្យការចូលដំណើរការរបស់ IMAP និង POP កម្មវិធីម៉ាស៊ីនបម្រើ។';
+$lang['imap_plugin_smtp_server_user'] = 'ម៉ាស៊ីនបម្រើ POP IMAP និងអ្នកប្រើ';
