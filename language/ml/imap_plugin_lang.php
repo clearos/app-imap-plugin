fbfd2ee237ce43471755f02968181093e6ebbbba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:43:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP, POP സെർവർ നയങ്ങള്';
+$lang['imap_plugin_app_description'] = 'IMAP, POP സെർവർ നയങ്ങള് IMAP, POP സെർവർ അപ്ലിക്കേഷൻ ആക്സസ്സ് നിയന്ത്രണം നൽകുന്നു.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP, POP സെർവർ ഉപയോക്തൃ';
