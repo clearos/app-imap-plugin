fd079d97f77badcbb77218974dd0a617cf7f570e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:36:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'Server Beleid IMAP en POP';
+$lang['imap_plugin_app_description'] = 'IMAP en POP Server Beleid voorsien toegangsbeheer vir die IMAP en POP Server app.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP en POP Server User';
