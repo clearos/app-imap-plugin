19de47ab98ac8c01e05e9cfb1585214309431bbc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'आईमैप और पॉप सर्वर नीतियाँ';
+$lang['imap_plugin_app_description'] = 'आईमैप और पॉप सर्वर नीतियां आईमैप और पॉप सर्वर अनुप्रयोग के लिए पहुँच नियंत्रण प्रदान करते हैं।';
+$lang['imap_plugin_smtp_server_user'] = 'आईमैप और पॉप सर्वर उपयोगकर्ता';
