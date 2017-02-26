de8e6338d229891d2fff99ded02236dea57f952b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:42:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್ ನೀತಿಗಳು';
+$lang['imap_plugin_app_description'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್ ನೀತಿಗಳು IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್ ಅಪ್ಲಿಕೇಶನ್ ಆಕ್ಸೆಸ್ ಕಂಟ್ರೋಲ್ ನೀಡಲಾಗುತ್ತದೆ.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ಮತ್ತು ಪಾಪ್ ಸರ್ವರ್ ಬಳಕೆದಾರ';
