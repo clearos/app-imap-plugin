ca6792a424c8261d934bfb7f8726d2c02c53ee61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:46:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP na POP Sera Server';
+$lang['imap_plugin_app_description'] = 'IMAP na POP Server Sera kutoa upatikanaji wa kudhibiti kwa programu IMAP na POP Server.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP na POP Server mtumiaji';
