0a33d0391136aeea7432c45c895a6382a639078a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP અને પીઓપી સર્વર નીતિઓ';
+$lang['imap_plugin_app_description'] = 'IMAP અને પીઓપી સર્વર નીતિઓ IMAP અને પીઓપી સર્વર એપ્લિકેશન માટે ઍક્સેસ નિયંત્રણ પૂરું પાડે છે.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP અને પીઓપી સર્વર વપરાશકર્તા';
