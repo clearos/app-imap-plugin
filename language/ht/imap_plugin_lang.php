888b443b0a9aff11d3682959b1f31691b7201a6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ak POP sèvè Règleman';
+$lang['imap_plugin_app_description'] = 'IMAP ak POP sèvè Règleman bay kontwòl aksè pou IMAP ak POP sèvè app a.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ak POP sèvè Itilizatè';
