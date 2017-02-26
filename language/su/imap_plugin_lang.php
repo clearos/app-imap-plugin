c1924e1372fd3e64bd33dc785851a306c5096b71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:46:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP jeung POP Kawijakan Server';
+$lang['imap_plugin_app_description'] = 'IMAP jeung POP Server Kawijakan nyadiakeun kadali aksés pikeun IMAP jeung POP Server app.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP jeung POP Server Pamaké';
