b9d7a1f929cf8396fbb933824a557be42c5c366c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:42:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP sy ny POP Server Policies';
+$lang['imap_plugin_app_description'] = 'IMAP sy ny POP Server Policies dia manome fahafahana ho an&#39;ny fanaraha-maso ny IMAP sy ny POP Server App.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP sy ny POP Server User';
