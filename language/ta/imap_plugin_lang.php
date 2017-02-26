5b7d4733e5791a844dee6df07e27982fe48847c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:46:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP மற்றும் POP சர்வர் கொள்கைகள்';
+$lang['imap_plugin_app_description'] = 'IMAP மற்றும் POP சர்வர் கொள்கைகள் IMAP மற்றும் POP சர்வர் பயன்பாட்டை கட்டுப்படுத்த உதவும்.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP மற்றும் POP சர்வர் பயனர்';
