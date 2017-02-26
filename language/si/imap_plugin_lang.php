9157e4bf89a15989bf9b8ef8b2e1c92566631c8c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:45:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP හා POP සේවාදායකය ප්රතිපත්ති';
+$lang['imap_plugin_app_description'] = 'IMAP හා POP සේවාදායකය ප්රතිපත්ති මෙම IMAP හා POP සේවාදායකය යෙදුම සඳහා ප්රවේශ පාලක ලබා දෙයි.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP හා POP සේවාදායකය පරිශීලක';
