a3e3abb5110936b708491484f1bffe0bd9a99b5d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP és POP szerver házirendje';
+$lang['imap_plugin_app_description'] = 'IMAP és POP szerver házirendje eléréskorlátozást az IMAP és POP szerver alkalmazás.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP és POP Server felhasználói';
