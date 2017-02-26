8a7d71e41ea89e1cdf5fa4a66f6f688df4f55fa8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:38:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'Condicions de l&#39;IMAP i POP del servidor';
+$lang['imap_plugin_app_description'] = 'IMAP i POP del servidor Les polítiques proporcionen control d&#39;accés per a l&#39;aplicació de servidor IMAP i POP.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP i POP Servidor d&#39;Usuari';
