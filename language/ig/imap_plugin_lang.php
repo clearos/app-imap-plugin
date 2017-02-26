9479afd272a92d54049fa28ac3fbb94f766e6242
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:41:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP na Pop Server Atumatu';
+$lang['imap_plugin_app_description'] = 'IMAP na Pop Server Atumatu-enye ohere akara maka IMAP na Pop Server ngwa.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP na Pop Server User';
