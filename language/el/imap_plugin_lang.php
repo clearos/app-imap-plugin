6b26cfe1fcdaafaae3d3383888b18b3ccddab4a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP και POP Πολιτικές διακομιστή';
+$lang['imap_plugin_app_description'] = 'IMAP και POP διακομιστή Πολιτικές παρέχει έλεγχο πρόσβασης για το IMAP και POP εφαρμογών διακομιστή.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP και POP Server χρήστη';
