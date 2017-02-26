ef5c4cc689886e1e8cc481a73d27eafc24b8f3f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:48:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP futhi POP Izinqubomgomo Server';
+$lang['imap_plugin_app_description'] = 'IMAP futhi POP Server Izinqubomgomo ukunikeza control ukufinyelela IMAP futhi POP Server lokusebenza.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP futhi POP Server Umsebenzisi';
