968527b0ff3afa649374d7105ebe7b3cf873af17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:38:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP和POP服务器策略';
+$lang['imap_plugin_app_description'] = 'IMAP和POP服务器策略提供对IMAP和POP服务器应用程序的访问控制。';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP和POP服务器用户';
