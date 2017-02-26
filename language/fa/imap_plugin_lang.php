28b37b2adb42688d9ca50221d4caff2eab4d315a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:44:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP و POP سیاست سرور';
+$lang['imap_plugin_app_description'] = 'IMAP و POP سرور سیاست کنترل دسترسی برای IMAP و POP برنامه سرور.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP و POP سرور کاربر';
