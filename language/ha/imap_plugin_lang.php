a214cfc2a7aa66ce637b07a5c67536c72f33204f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:40:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP da POP Server Asiri';
+$lang['imap_plugin_app_description'] = 'IMAP da POP Server Asiri samar da damar iko da IMAP, da POP Server app.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP da POP Server User';
