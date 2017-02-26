01114d677ecee6a14dc52d70e038bb35290ee32f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:48:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'ימאַפּ און POP סערווירער פּאָליסיעס';
+$lang['imap_plugin_app_description'] = 'ימאַפּ און POP סערווירער פּאַלאַסיז צושטעלן צוטריט קאָנטראָל פֿאַר די ימאַפּ און POP סערווירער אַפּ.';
+$lang['imap_plugin_smtp_server_user'] = 'ימאַפּ און POP סערווירער באַניצער';
