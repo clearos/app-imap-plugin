a49ce4ce559204451c33be489e45423be791a1e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:39:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/imap_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['imap_plugin_app_name'] = 'IMAP ja POP-palvelin säännöt';
+$lang['imap_plugin_app_description'] = 'IMAP ja POP Server käytännöt tarjoavat kulunvalvonta IMAP ja POP-palvelin sovellus.';
+$lang['imap_plugin_smtp_server_user'] = 'IMAP ja POP-palvelin Käyttäjä';
